ccd76940e6ec6af16dabf9805f38714d77f444c3
Mitchell.Morgan <mitchemorgan@gmail.com>
2024-01-29 18:51:02 -0700

    practicing flex on native

--- ReactPractice/App.js
+++ ReactPractice/App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  console.log("app executed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.text}>About</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Projects</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Design</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>Contact</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    color: "white",
+    backgroundColor: "gray",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  box: {
+    backgroundColor: "black",
+    height: "20%",
+    width: "90%",
+    // borderRadius: "30",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    // fontSize: "100",
   },
 });
